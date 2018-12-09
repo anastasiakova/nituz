fae6ede3d91f3687707b5ba80cdb8d6cc39e5af1
kovalkov <kovalkov@post.bgu.ac.il>
2018-12-09 18:14:32 +0200

    message is in search controller

--- src/main/java/Controller/CreateController.java
+++ src/main/java/Controller/CreateController.java
@@ -14,19 +14,8 @@ public class CreateController {
         ISQLable newUser = new User(username, pwd, birthday, privateName, lastName, city, bankAcount, creditCard, id);
         sqlModel.insertRecordToTable(Tables.TBL_USERS.toString().toLowerCase(), newUser);
     }
-
-    public String getUserCreatedMassage(String userText) {
-        String[] users = new String[TblFields.enumDict.get("userFields").size()];
-        users[0] = userText;
-        String ansSelect = sqlModel.selectFromTable(Tables.TBL_USERS, users);
-        String[] arrAns = ansSelect.split(",");
-        return "User Created:\n\n" +
-                        "User Name: " + arrAns[0] + "\n"
-                        + "Password: " + arrAns[1].substring(1)
-
-                        + "\nB-day: " + arrAns[2].substring(1)
-                        + "\nFirst Name: " + arrAns[3].substring(1)
-                        + "\nLast Name: " + arrAns[4].substring(1)
-                        + "\nCity: " + arrAns[5].substring(1, arrAns[5].length() - 2);
+    public String getUserCreatedMassage(String userText,Boolean withPssword){
+        return new SearchController().getMassageAfterChange(userText,withPssword);
     }
+
 }
--- src/main/java/Controller/SearchController.java
+++ src/main/java/Controller/SearchController.java
@@ -1,4 +1,42 @@
 package Controller;
 
+import Model.SQLModel;
+import Model.Tables;
+import Model.TblFields;
+
 public class SearchController {
+
+    SQLModel sqlModel;
+
+    public SearchController() {
+        this.sqlModel = SQLModel.GetInstance();
+    }
+
+    public String getMassageAfterChange(String userText, boolean withPassword) {
+        String[] users = new String[TblFields.enumDict.get("userFields").size()];
+        users[0] = userText;
+        String ansSelect = sqlModel.selectFromTable(Tables.TBL_USERS, users);
+
+        if (ansSelect == "") {
+            return "User does not exist in the system! ";
+        } else {
+            String[] arrAns = ansSelect.split(",");
+            if (withPassword) {
+                return "User Name: " + arrAns[0] + "\n"
+                        + "Password: " + arrAns[1].substring(1)
+                        + "\nB-day: " + arrAns[2].substring(1)
+                        + "\nFirst Name: " + arrAns[3].substring(1)
+                        + "\nLast Name: " + arrAns[4].substring(1)
+                        + "\nCity: " + arrAns[5].substring(1, arrAns[5].length() - 2);
+            }
+            else {
+                return "User Name: " + arrAns[0] + "\n"
+                        + "\nB-day: " + arrAns[2].substring(1)
+                        + "\nFirst Name: " + arrAns[3].substring(1)
+                        + "\nLast Name: " + arrAns[4].substring(1)
+                        + "\nCity: " + arrAns[5].substring(1, arrAns[5].length() - 2);
+            }
+        }
+    }
 }
+
