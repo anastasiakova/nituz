328af3724be3130487967ad58b7bb2f2b8fc58a6
anastasiakova <kovalkov@post.bgu.ac.il>
2018-12-11 19:41:22 +0200

    search controller

--- src/main/java/Controller/SearchController.java
+++ src/main/java/Controller/SearchController.java
@@ -30,18 +30,11 @@ public class SearchController {
             String[] arrAns = ansSelect.split(",");
             if (withPassword) {
                 return "User Name: " + arrAns[0] + "\n"
-                        + "Password: " + arrAns[1].substring(1)
-                        + "\nB-day: " + arrAns[2].substring(1)
-                        + "\nFirst Name: " + arrAns[3].substring(1)
-                        + "\nLast Name: " + arrAns[4].substring(1)
-                        + "\nCity: " + arrAns[5].substring(1, arrAns[5].length() - 2);
+                        + "Password: " + arrAns[1].substring(1);
+
             }
             else {
-                return "User Name: " + arrAns[0] + "\n"
-                        + "\nB-day: " + arrAns[2].substring(1)
-                        + "\nFirst Name: " + arrAns[3].substring(1)
-                        + "\nLast Name: " + arrAns[4].substring(1)
-                        + "\nCity: " + arrAns[5].substring(1, arrAns[5].length() - 2);
+                return "User Name: " + arrAns[0] + "\n";
             }
         }
     }
