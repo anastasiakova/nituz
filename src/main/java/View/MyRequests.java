e6092310a56af66a4ec1eef209af9ecdf2eac65b
anastasiakova <kovalkov@post.bgu.ac.il>
2018-12-11 09:55:17 +0200

    search controller inits bigModel serch request vacation functions

--- src/main/java/View/MyRequests.java
+++ src/main/java/View/MyRequests.java
@@ -1,40 +1,45 @@
 package View;
 
 import Controller.SearchController;
-import Model.ISQLable;
-import Model.Vacation;
+import Model.VactaionAndRequest;
+import javafx.collections.ObservableList;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.Date;
-
 public class MyRequests {
-    public javafx.scene.control.TableView<ISQLable> reqTable;
-    public javafx.scene.control.TableColumn<ISQLable,String> destination;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> startDate;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> endDate;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> sallerUserName;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> answer;//request
+    SearchController searchController;
+    public javafx.scene.control.TableView<VactaionAndRequest> reqTable;
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> destination;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> startDate;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> endDate;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> sallerUserName;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> answer;//request
 
-    public javafx.scene.control.TableView<ISQLable> reqForMeTable;
-    public javafx.scene.control.TableColumn<ISQLable,String> myVecDestination;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> myVecStartDate;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> myVecEndDate;//vecation
-    public javafx.scene.control.TableColumn<ISQLable,String> buyerUserName;//request
-    public javafx.scene.control.TableColumn<ISQLable,String> sallerAnswer;//dont know should be changeable
-    public MyRequests(){ };
+    public javafx.scene.control.TableView<VactaionAndRequest> reqForMeTable;
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> myVecDestination;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> myVecStartDate;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> myVecEndDate;//vecation
+    public javafx.scene.control.TableColumn<VactaionAndRequest,String> buyerUserName;//request
+
+    public MyRequests(SearchController searchController){
+        this.searchController = searchController;
+    }
 
     public void init() {
-        destination.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("_destination"));
-        startDate.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("__startDate"));
-        endDate.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("_endDate"));
-        sallerUserName.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("_ownerID"));
-        answer.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("r_answer"));
+        destination.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("_destination"));
+        startDate.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("__startDate"));
+        endDate.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("_endDate"));
+        sallerUserName.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("_ownerID"));
+        answer.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("r_answer"));
 
-        myVecDestination.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("_destination"));
-        myVecStartDate.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("__startDate"));
-        myVecEndDate.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("_endDate"));
-        buyerUserName.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("r_buyerID"));
-        sallerAnswer.setCellValueFactory(new PropertyValueFactory<ISQLable,String>("r_answer"));
+        myVecDestination.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("_destination"));
+        myVecStartDate.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("__startDate"));
+        myVecEndDate.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("_endDate"));
+        buyerUserName.setCellValueFactory(new PropertyValueFactory<VactaionAndRequest,String>("r_buyerID"));
+
+        ObservableList<VactaionAndRequest> myRequests = searchController.getMyRequests();
+        reqTable.setItems(myRequests);
+        ObservableList<VactaionAndRequest> requestsForMe = searchController.getToAnswerRequests();
+        reqForMeTable.setItems(requestsForMe);
     }
  }
 
